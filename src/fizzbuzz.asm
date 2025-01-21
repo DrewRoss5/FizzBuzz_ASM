0964e442d6c24f71e9d36cfd6257f5ed69b7b9eb
Drew <145530440+DrewRoss5@users.noreply.github.com>
2025-01-21 10:27:50 -0700

    Simplify printing via macros

--- src/fizzbuzz.asm
+++ src/fizzbuzz.asm
@@ -1,64 +1,66 @@
 # registers:
 # t0: target number
 # t1: current itteration number
-# t2: the modulo of t1 3, 5, or 15
+# t2: the modulo of t0 and either three or five
 # t3: the constant 3
 # t4: keeps track of if fizz or buzz was printed
 # t5: the constant 5
-# t6: the constant 15
+
+.macro print_int(%reg)
+	li a7, 1
+	add a0, %reg, x0
+	ecall
+.end_macro 
+
+.macro print_str(%str)
+	li a7, 4
+	la a0, %str
+	ecall
+.end_macro 
 
 .data
 prompt: .asciz "Target: "
-fizz: .asciz "Fizz\n"
-buzz: .asciz "Buzz\n"
-fizzbuzz: .asciz "Fizzbuzz\n"
+fizz: .asciz "Fizz"
+buzz: .asciz "Buzz"
 completed: .asciz "\nCompleted!\n"
 newline: .asciz "\n"
 
 .text
-addi t3, x9, 3 # t3 = 3
-addi t5, x0, 5 # t5 = 5
-addi t6, x0, 15 # t6 = 15
+addi t3, zero, 3 # t3 = 3
+addi t5, zero, 5 # t5 = 5
 
-li a7, 4
-la a0, prompt
-ecall # print the initial promp
+print_str(prompt)
 li a7, 5
 ecall # read an integer to a0
 addi t0, a0, 1 # t0 = a0 + 1
 
-
-loop:
+init_loop: 
 	addi t1, t1, 1 # t1++
 	beq t0, t1, exit # if (t1 == t2) goto exit
-	la a0, fizzbuzz # load "Fizzbuzz" into a0
-	rem t2, t1, t6 # t3 = t1 % 15
-	beq t2, x0, print_str # if (t2 == 0) goto print_str
-	la a0, buzz #load "Buzz" into a0
-	rem t2, t1, t5 # t3 = t1 % 5
-	beq t2, x0, print_str # if (t3 == 0) goto print_str
-	la a0, fizz #load "Fizz" into a0
-	rem t2, t1, t3 # t3 = t1 % 5
-	beq t2, x0, print_str # if (t3 == 0) goto print_str
-	b print_num # print the current number
+	print_str(newline)
+	add t4, x0, x0 # t4 = 0
+	rem t2, t1, t3 # t2 = t1 % 3
+	beq t2, x0, print_fizz # if (!t3) goto print_fizz
+	check_buzz:
+		rem t2, t1, t5 # t2 = t1 % 5
+		beq t2, x0, print_buzz # if (!t2) 
+	beq t4, x0, print_num # if (!t4) goto print_num 
+	b init_loop
+
+print_fizz:
+	print_str(fizz)
+	addi t4, t4, 1
+	b check_buzz
 	
-print_str:
-	li a7, 4 # load the print string syscall code into a7
-	ecall # print whatever string is pointed to by a0 
-	b loop
+print_buzz: 
+	print_str(buzz)
+	b init_loop
 	
 print_num:
-	li a7, 1 # load the print integer syscall into a7
-	add a0, t1, x0 # a0 = t1
-	ecall # print the content of t1
-	li a7, 4 # load the print string syscall code into a7
-	la a0, newline # a0 = newline
-	ecall # print the new line
-	b loop
-
+	print_int(t1)
+	b init_loop
+	
 exit:
-	li a7, 4
-	la a0, completed
-	ecall
+	print_str(completed)
 	li a7, 10
 	ecall
